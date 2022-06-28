5b354e9450d85a94a3913254d21353f423204f07
root <sylyac2000@gmail.com>
2022-06-27 22:35:39 -0700

    Write an empty class Rectangle that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module is about 0-rectangle
+Write an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """Rectangle empty class"""
+    pass
