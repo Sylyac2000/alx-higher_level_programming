1984331128a84f122213dd2d914d216a38146da3
root <sylyac2000@gmail.com>
2022-07-12 10:26:25 -0700

    Update the class Base by adding the static method def from_json_string(json_string): that returns the list of the JSON string representation json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,3 +44,10 @@ class Base:
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns Python obj from JSON string"""
+        if json_string is None or len(json_string) == 0:
+            json_string = "[]"
+        return json.loads(json_string)
