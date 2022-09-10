3d5db9c498597c6bc1955e5cba2b977d7b5709d3
root <sylyac2000@gmail.com>
2022-09-10 03:15:05 -0700

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module is about  a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa where name matches the argument
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    if len(argv) != 5:
+        print("format is:0-select_states.py username password database state")
+    else:
+        list_dbparam = argv[1:]
+        # Open database connection
+        db = MySQLdb.connect(host='localhost', user=list_dbparam[0],
+                             passwd=list_dbparam[1], db=list_dbparam[2],
+                             port=3306)
+
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor()
+
+        sql = """SELECT * FROM states WHERE states.name LIKE '{0}'
+                 ORDER BY states.id ASC""".format(list_dbparam[3])
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+        # disconnect from server
+        db.close()
