0d2b0f4063f425672d16ebee4d14886c614d0c54
root <sylyac2000@gmail.com>
2022-09-10 23:18:14 -0700

    Write a python file that contains the class definition of a State and an instance Base = declarative_base() updated

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,12 +7,15 @@ of a State and an instance Base = declarative_base()
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.dialects import mysql
 
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    Class State; instance of Base
+    representing MySQL table "states"
+    """
     __tablename__ = 'states'
-    id = Column(mysql.INTEGER(11), primary_key=True)
+    id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
