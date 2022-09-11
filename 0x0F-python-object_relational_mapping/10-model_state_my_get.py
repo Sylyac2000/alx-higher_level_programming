79587c8efbc89d19dcbfe4c4e92217b175071fe7
root <sylyac2000@gmail.com>
2022-09-11 01:36:47 -0700

    Write a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module is about listing all states from
+a mysql database table called states
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    if len(argv) != 5:
+        print("format is:0-select_states.py username password database")
+    else:
+        list_dbparam = argv[1:]
+        # Open database connection
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                               format(list_dbparam[0], list_dbparam[1],
+                                      list_dbparam[2]), pool_pre_ping=True)
+
+        # create session
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        state = session.query(State).filter_by(name=list_dbparam[3]).first()
+        if state:
+            print("{:d}".format(state.id))
+        else:
+            print("Not found")
+
+        session.close()
