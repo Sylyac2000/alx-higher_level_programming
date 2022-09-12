a2e4517f5b2a62d5e8f7592f5f4418cec2c62926
root <sylyac2000@gmail.com>
2022-09-11 22:30:35 -0700

    Write a Python file similar to model_state.py named model_city.py that contains the class definition of a City.

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module is about a python file that contains the class definition
+of a City and an instance Base = declarative_base()
+"""
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    Class City; instance of Base
+    representing MySQL table "city"
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, nullable=False, primary_key=True)  # autoincrements
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
+
