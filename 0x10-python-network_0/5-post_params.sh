44a0ad741e460d0b04ba69050aeaac00f5c963d7
root <sylyac2000@gmail.com>
2022-09-22 22:31:36 -0700

    Write a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject =I will always be here for PLD" "$1"
+#a Bash script that sends POST to URL
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
