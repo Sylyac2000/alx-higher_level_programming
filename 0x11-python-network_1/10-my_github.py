5d6682d446475a3db23ff870885c6cfd118761b6
root <sylyac2000@gmail.com>
2022-09-24 14:40:14 -0700

    github account

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     import requests
     from requests.auth import HTTPBasicAuth
     from sys import argv
-    url = 'https://api.github.com/users/{}'.format(argv[1])
+
+    url = 'https://api.github.com/user'
     r = requests.get(url,
                      auth=HTTPBasicAuth(argv[1], argv[2]))
     print(r.json().get('id'))
