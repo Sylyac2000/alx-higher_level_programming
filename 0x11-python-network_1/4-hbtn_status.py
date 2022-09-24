2d3c7b3f8ce89c3313fd88bff1de83ba6a155de1
root <sylyac2000@gmail.com>
2022-09-23 19:30:49 -0700

    a Python script that fetches https://alx-intranet.hbtn.io/status using requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches https://alx-intranet.hbtn.io/status
+using requests
+"""
+
+if __name__ == "__main__":
+    import requests
+
+    thehtml = ""
+    theurl = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(theurl)
+    print(response.text)
+    print('Body response:')
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
