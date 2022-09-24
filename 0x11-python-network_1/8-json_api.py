044bb2533eb82c312c00481c0bb58121480c7936
root <sylyac2000@gmail.com>
2022-09-23 22:42:42 -0700

    Write a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    if len(argv) == 3:
+        thehtml = ""
+        listargs = argv[1:]
+        theurl = listargs[0]
+        if listargs[1]:
+            donnees = {'q': listargs[1]}
+        else
+            donnees = {'q': ''}
+    response = requests.post(theurl, donnees)
+
+    try:
+        dicdata = response.json()
+        if dicdata:
+            print("[{}] {}".format(dicdata.get('id'), dicdata.get('name')))
+        else:
+            print("No result")
+    except ValueError as e:
+        print("Not a valid JSON")
