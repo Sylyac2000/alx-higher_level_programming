13b3ececebec6689795de93fbf924a80b52db79b
SYLLA Yacouba <sylyac2000@gmail.com>
2022-12-16 08:18:28 -0800

    15. City relationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -20,4 +20,4 @@ class City(Base):
     __tablename__ = "cities"
     id = Column(Integer, nullable=False, primary_key=True)  # autoincrements
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
